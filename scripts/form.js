075f44d4bd799dcdb1d918690d8ef3b108931021
MalenaMoreno <malenamoreno514@gmail.com>
2023-10-24 10:25:58 -0300

    commit

---
+++ scripts/form.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    //Datos
+    const fullname = document.getElementById('fullname').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phoneNumber = document.getElementById('phone-number').value.trim();
+    const source = document.getElementById('source').value; 
+    const comment = document.getElementById('comment').value.trim();
+
+
+    if(!fullname.includes(" ")){
+        alert("Ingresa tu nombre y apellido")
+        return
+    }
+
+    if(!email.includes("@")){
+        alert("Ingresa un mail valido")
+        return
+    }
+
+    if(comment == ""){
+        alert("Escribí tu consulta/comentario")
+        return
+    }
+
+    // const consultas = {
+    //     name: fullname,
+    //     email: email,
+    //     phoneNumber: phoneNumber,
+    //     source: source,
+    //     comment: comment,
+    // }
+
+    console.log("Nombre:", fullname);
+    console.log("Correo:", email);
+    console.log("Número de teléfono:", phoneNumber);
+    console.log("Comentario/Consulta", comment);
+    console.log("Como nos conoció:", source);
+
+});
---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const menuButton = document.getElementById("menu-icon")
+const nav = document.getElementById("nav")
+const closeButton = document.getElementById("close")
+
+menuButton.addEventListener("click", function(){
+    nav.style.display = "inline-block"
+    closeButton.style.display = "inline-block"
+    menuButton.style.display = "none"
+})
+
+closeButton.addEventListener("click", function(){
+    nav.style.display = ""
+    menuButton.style.display = "" 
+    closeButton.style.display = ""
+})
